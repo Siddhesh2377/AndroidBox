8e5850766ca88b0bf468770944b1d6d928769e47
darkengine <darkwildahcker@gmail.com>
2024-09-07 16:43:17 +0530

    code update

--- app/src/main/java/com/dark/androidbox/MainActivity.java
+++ app/src/main/java/com/dark/androidbox/MainActivity.java
@@ -27,11 +27,9 @@ import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
-    GysoTreeView treeView;
-
-    TreeViewAdapter<NodeData> adapter;
+    private ActivityMainBinding binding;
+    private GysoTreeView treeView;
+    private TreeViewAdapter<NodeData> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,241 +37,55 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         treeView = binding.nodeView.treeview;
         setupSyntaxHighlighter();
 
         binding.btn.setOnClickListener(view -> {
-            if (!binding.code.getText().toString().isEmpty())
-                loadJava(new StringBuilder(binding.code.getText().toString()));
+            String codeText = binding.code.getText().toString();
+            if (!codeText.isEmpty()) {
+                loadJava(new StringBuilder(codeText));
+            }
         });
-
     }
-
 
     private void loadJava(StringBuilder code) {
         Lexer lexer = new Lexer(code);
         adapter = new NodeViewAdapter();
+        initializeTreeView();
 
-        //binding.txt.setText(lexer.getFields().get(0).getVariables().get(0).getNameAsString() + lexer.getFields().get(0).getVariables().get(0).getType().asString());
-
-
-        treeView.setAdapter(adapter);
-        treeView.setTreeLayoutManager(new BoxDownTreeLayoutManager(this, 20, 20, new SmoothLine()));
-
-
-        NodeModel<NodeData> node0 = new NodeModel<>(new NodeData(lexer.getClasses().get(0).getNameAsString(), lexer.getClasses().get(0).toString(), NodeTypes.CLASSES));
+        NodeModel<NodeData> node0 = createNode(lexer.getClasses().get(0), NodeTypes.CLASSES);
         TreeModel<NodeData> treeModel = new TreeModel<>(node0);
-        NodeModel<NodeData> node1 = new NodeModel<>(new NodeData(lexer.getMethods().get(0).getNameAsString(), lexer.getMethods().get(0).toString(), NodeTypes.METHODS));
-        NodeModel<NodeData> node2 = new NodeModel<>(new NodeData(lexer.getFields().get(0).getVariables().get(0).getNameAsString(), lexer.getFields().get(0).getVariables().get(0).getInitializer().get().toString(), NodeTypes.VARIABLES));
 
+        treeModel.addNode(
+                createNode(lexer.getMethods().get(0), NodeTypes.METHODS),
+                createNode(lexer.getFields().get(0).getVariables().get(0), NodeTypes.VARIABLES)
+        );
 
-        treeModel.addNode(node0, node1, node2);
         adapter.setTreeModel(treeModel);
-
-        binding.btn.setVisibility(View.GONE);
-        binding.code.setVisibility(View.GONE);
-
-        new Handler(getMainLooper()).postDelayed(() -> treeView.getEditor().focusMidLocation(), 2000);
+        updateUI();
     }
 
+    private void initializeTreeView() {
+        treeView.setAdapter(adapter);
+        treeView.setTreeLayoutManager(new BoxDownTreeLayoutManager(this, 20, 20, new SmoothLine()));
+    }
 
-    private StringBuilder code() {
-        return new StringBuilder("package com.dark.androidbox.Adpaters;\n" +
-                "\n" +
-                "import android.annotation.SuppressLint;\n" +
-                "import android.app.Activity;\n" +
-                "import android.graphics.Color;\n" +
-                "import android.text.Spannable;\n" +
-                "import android.text.SpannableStringBuilder;\n" +
-                "import android.text.style.ForegroundColorSpan;\n" +
-                "import android.util.Log;\n" +
-                "import android.view.LayoutInflater;\n" +
-                "import android.view.View;\n" +
-                "import android.view.ViewGroup;\n" +
-                "import android.widget.LinearLayout;\n" +
-                "import android.widget.RelativeLayout;\n" +
-                "import android.widget.TextView;\n" +
-                "\n" +
-                "import androidx.annotation.NonNull;\n" +
-                "import androidx.fragment.app.FragmentManager;\n" +
-                "\n" +
-                "import com.dark.androidbox.Editor.Codes;\n" +
-                "import com.dark.androidbox.Fragments.EditorFragment;\n" +
-                "import com.dark.androidbox.R;\n" +
-                "import com.dark.androidbox.System.NodeEvents;\n" +
-                "import com.dark.androidbox.builder.LogicBuilder;\n" +
-                "import com.dark.androidbox.databinding.CodeNodesBinding;\n" +
-                "import com.google.android.material.button.MaterialButton;\n" +
-                "import com.google.android.material.textview.MaterialTextView;\n" +
-                "import com.gyso.treeview.TreeViewEditor;\n" +
-                "import com.gyso.treeview.adapter.DrawInfo;\n" +
-                "import com.gyso.treeview.adapter.TreeViewAdapter;\n" +
-                "import com.gyso.treeview.adapter.TreeViewHolder;\n" +
-                "import com.gyso.treeview.line.BaseLine;\n" +
-                "import com.gyso.treeview.model.NodeModel;\n" +
-                "\n" +
-                "import java.util.ArrayList;\n" +
-                "\n" +
-                "public class CodeAdapter extends TreeViewAdapter<Codes> {\n" +
-                "\n" +
-                "\n" +
-                "    public Activity ctx;\n" +
-                "    NodeEvents events;\n" +
-                "\n" +
-                "    FragmentManager manager;\n" +
-                "\n" +
-                "    TreeViewEditor editor;\n" +
-                "\n" +
-                "    public CodeAdapter(FragmentManager manager, Activity activity, NodeEvents events, TreeViewEditor editor) {\n" +
-                "        this.ctx = activity;\n" +
-                "        this.events = events;\n" +
-                "        this.manager = manager;\n" +
-                "        this.editor = editor;\n" +
-                "    }\n" +
-                "\n" +
-                "    @Override\n" +
-                "    public TreeViewHolder<Codes> onCreateViewHolder(@NonNull ViewGroup viewGroup, NodeModel<Codes> model) {\n" +
-                "        CodeNodesBinding nodeBinding = CodeNodesBinding.inflate(LayoutInflater.from(viewGroup.getContext()), viewGroup, false);\n" +
-                "        return new TreeViewHolder<>(nodeBinding.getRoot(), model);\n" +
-                "    }\n" +
-                "\n" +
-                "    @SuppressLint(\"SetTextI18n\")\n" +
-                "    @Override\n" +
-                "    public void onBindViewHolder(@NonNull TreeViewHolder<Codes> holder) {\n" +
-                "        View items = holder.getView();\n" +
-                "\n" +
-                "        LinearLayout head_node = items.findViewById(R.id.head_node);\n" +
-                "\n" +
-                "        RelativeLayout cardInfo = items.findViewById(R.id.card_info);\n" +
-                "\n" +
-                "        TextView label = items.findViewById(R.id.label_codeBlock);\n" +
-                "\n" +
-                "        TextView txt_info = items.findViewById(R.id.txt_info);\n" +
-                "\n" +
-                "        MaterialTextView node_id = items.findViewById(R.id.node_id);\n" +
-                "\n" +
-                "        MaterialButton delNode = items.findViewById(R.id.delNode);\n" +
-                "\n" +
-                "        MaterialButton codeNode = items.findViewById(R.id.btn_code);\n" +
-                "\n" +
-                "        NodeModel<Codes> nodeObj = holder.getNode();\n" +
-                "\n" +
-                "        final Codes blockData = nodeObj.value;\n" +
-                "\n" +
-                "        cardInfo.setVisibility(View.GONE);\n" +
-                "\n" +
-                "        items.setOnClickListener(view -> events.NodeOnClick(blockData.itemId));\n" +
-                "\n" +
-                "        head_node.setOnLongClickListener(v -> {\n" +
-                "            cardInfo.setVisibility(!(cardInfo.getVisibility() == View.VISIBLE) ? View.VISIBLE : View.GONE);\n" +
-                "\n" +
-                "            return false;\n" +
-                "        });\n" +
-                "\n" +
-                "        delNode.setOnClickListener(view -> editor.removeNode(holder.getNode()));\n" +
-                "\n" +
-                "        codeNode.setOnClickListener(view -> {\n" +
-                "\n" +
-                "        });\n" +
-                "\n" +
-                "        label.setText(blockData.label);\n" +
-                "\n" +
-                "        if (blockData.itemId == 0) {\n" +
-                "            String data = String.valueOf(setUpClassInfo(new LogicBuilder(EditorFragment.sampleCode())));\n" +
-                "\n" +
-                "            SpannableStringBuilder colorant = new SpannableStringBuilder(data);\n" +
-                "\n" +
-                "            SetUpColor(colorant, data, \"Type\", \"#8EBBFF\");\n" +
-                "            SetUpColor(colorant, data, \"Returns\", \"#8EBBFF\");\n" +
-                "            SetUpColor(colorant, data, \"Extends\", \"#8EBBFF\");\n" +
-                "            SetUpColor(colorant, data, \"Interface\", \"#8EBBFF\");\n" +
-                "            SetUpColor(colorant, data, \"Null\", \"#FF8E8E\");\n" +
-                "            SetUpColor(colorant, data, \"Class\", \"#BBB0FF\");\n" +
-                "\n" +
-                "            txt_info.setText(colorant);\n" +
-                "\n" +
-                "        } else {\n" +
-                "            if (blockData.itemId == 1) {\n" +
-                "                txt_info.setText(\"Click Here To Add Var\");\n" +
-                "                txt_info.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);\n" +
-                "            } else {\n" +
-                "                if (blockData.itemId == 2) {\n" +
-                "                    txt_info.setText(\"Click Here To Add Methods\");\n" +
-                "                    txt_info.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);\n" +
-                "                }\n" +
-                "            }\n" +
-                "        }\n" +
-                "\n" +
-                "        node_id.setText(\"\" + blockData.type);\n" +
-                "\n" +
-                "        Log.d(\"System Info\", String.valueOf(new LogicBuilder(EditorFragment.sampleCode())));\n" +
-                "    }\n" +
-                "\n" +
-                "    public StringBuilder setUpClassInfo(LogicBuilder builder) {\n" +
-                "\n" +
-                "        StringBuilder Type, Returns, Inputs = new StringBuilder(\"\"), data = new StringBuilder(\"\");\n" +
-                "\n" +
-                "\n" +
-                "        ArrayList<StringBuilder> Interfaces = builder.getClassImplementation();\n" +
-                "        StringBuilder Extends = builder.getClassExtends().get(0);\n" +
-                "\n" +
-                "        if (Extends.length() != 0) {\n" +
-                "            Inputs = new StringBuilder(\"Extends : \".concat(Extends.toString()));\n" +
-                "            if (Interfaces.size() != 0) {\n" +
-                "                Inputs.append(\"\\nInterface : \".concat(String.join(\", \", Interfaces)));\n" +
-                "            }\n" +
-                "        }\n" +
-                "        Type = new StringBuilder(\"Type : Class \\n\");\n" +
-                "        Returns = new StringBuilder(\"Returns : Null \\n\");\n" +
-                "\n" +
-                "        data.append(Type).append(Returns).append(Inputs);\n" +
-                "\n" +
-                "        return data;\n" +
-                "    }\n" +
-                "\n" +
-                "    @Override\n" +
-                "    public BaseLine onDrawLine(DrawInfo drawInfo) {\n" +
-                "        return null;\n" +
-                "    }\n" +
-                "\n" +
-                "    public void SetUpColor(SpannableStringBuilder colorant, String data, String word, String color) {\n" +
-                "        ForegroundColorSpan txtColor1 = new ForegroundColorSpan(Color.parseColor(color));\n" +
-                "\n" +
-                "        int start1 = data.indexOf(word);\n" +
-                "        if (start1 != -1) {\n" +
-                "            int end1 = start1 + word.length();\n" +
-                "            colorant.setSpan(txtColor1, start1, end1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);\n" +
-                "        }\n" +
-                "    }\n" +
-                "}");
+    private NodeModel<NodeData> createNode(Object item, NodeTypes type) {
+        return new NodeModel<>(new NodeData(item.toString(), item.toString(), type));
+    }
+
+    private void updateUI() {
+        binding.btn.setVisibility(View.GONE);
+        binding.code.setVisibility(View.GONE);
+        new Handler(getMainLooper()).postDelayed(() -> treeView.getEditor().focusMidLocation(), 2000);
     }
 
     private void setupSyntaxHighlighter() {
         binding.code.addTextChangedListener(new TextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
-                // Remove existing spans
-                ForegroundColorSpan[] spans = s.getSpans(0, s.length(), ForegroundColorSpan.class);
-                for (ForegroundColorSpan span : spans) {
-                    s.removeSpan(span);
-                }
-
-                // Java syntax patterns
-                String keywordPattern = "\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\\b";
-                String commentPattern = "//.*|/\\*(?:.|[\\n\\r])*?\\*/";
-                String stringPattern = "\"(.*?)\"";
-                String numberPattern = "\\b\\d+\\b";  // Numbers
-                String annotationPattern = "@\\w+";  // Annotations like @Override
-                String primitiveTypePattern = "\\b(int|char|float|double|boolean|long|short|byte)\\b";  // Primitive types
-
-                // Apply patterns with colors
-                applyPattern(s, keywordPattern, Color.parseColor("#186fa1"));  // Java keywords
-                applyPattern(s, commentPattern, Color.GRAY);  // Comments
-                applyPattern(s, stringPattern, Color.parseColor("#32872f"));  // Strings
-                applyPattern(s, numberPattern, Color.parseColor("#d35400"));  // Numbers (orange)
-                applyPattern(s, annotationPattern, Color.parseColor("#9b59b6"));  // Annotations (purple)
-                applyPattern(s, primitiveTypePattern, Color.parseColor("#2c3e50"));  // Primitive types (dark blue)
+                removeExistingSpans(s);
+                highlightSyntax(s);
             }
 
             @Override
@@ -286,6 +98,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void removeExistingSpans(Editable s) {
+        ForegroundColorSpan[] spans = s.getSpans(0, s.length(), ForegroundColorSpan.class);
+        for (ForegroundColorSpan span : spans) {
+            s.removeSpan(span);
+        }
+    }
+
+    private void highlightSyntax(Editable s) {
+        applyPattern(s, "\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|goto|if|implements|import|instanceof|int|interface|long|native|new|null|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\\b", Color.parseColor("#186fa1"));
+        applyPattern(s, "//.*|/\\*(?:.|[\\n\\r])*?\\*/", Color.GRAY);
+        applyPattern(s, "\"(.*?)\"", Color.parseColor("#32872f"));
+        applyPattern(s, "\\b\\d+\\b", Color.parseColor("#d35400"));
+        applyPattern(s, "@\\w+", Color.parseColor("#9b59b6"));
+        applyPattern(s, "\\b(int|char|float|double|boolean|long|short|byte)\\b", Color.parseColor("#2c3e50"));
+    }
+
     private void applyPattern(Editable s, String pattern, int color) {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(s);
@@ -293,6 +121,4 @@ public class MainActivity extends AppCompatActivity {
             s.setSpan(new ForegroundColorSpan(color), m.start(), m.end(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
     }
-
-
 }
